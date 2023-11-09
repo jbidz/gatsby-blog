8f9f74b9c310e72271d0ebc58b31ef0e48fa5c81
John Denver Bidong <johndenverbidong1@gmail.com>
2023-11-09 20:52:22 +0800

    chaning image path to local path

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
       <StaticImage
         className="rounded-xl"
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        src="../images/dog.jfif"
       />
     </Layout>
   );
