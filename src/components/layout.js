14f086217be76eb572558510be8beda63b94888d
John Denver Bidong <johndenverbidong1@gmail.com>
2023-11-09 19:46:36 +0800

    Initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className="max-w-3xl mx-auto">
+      <nav className="py-6">
+        <ul className="flex gap-5">
+          <li>
+            <Link to="/" className="underline">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="underline">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className="text-4xl mb-4 font-black">{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
